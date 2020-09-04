96fab920062bfefb9d5b16c90592c6128a48560f
Julian Wood <jrwood@amazon.com>
2020-09-04 19:22:28 +0100

    Add date format change

--- getblogpost.js
+++ getblogpost.js
@@ -1,6 +1,23 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+    return [Month, month, day].join('-');
+
+}
+
 // var url = 'https://aws.amazon.com/blogs/compute/building-well-architected-serverless-applications-introduction/';
 var url = process.argv[2];
 request(url, function (error, response, responseHtml) {
@@ -39,7 +56,8 @@ request(url, function (error, response, responseHtml) {
     }
 
     if ($date) {
-        blogobj.date = $date;
+        const formatter = new Intl.DateTimeFormat('en', { month: "short", day: "2-digit", year: "numeric" });
+        blogobj.date = formatter.format(new Date($date));
     }
 
     if ($link) {
