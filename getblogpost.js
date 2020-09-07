3d1c7d95a929ce6d55024bdbc3d939b661c86b4e
Julian Wood <jrwood@amazon.com>
2020-09-07 11:07:51 +0100

    Change to output empty values rather than skip

--- getblogpost.js
+++ getblogpost.js
@@ -14,49 +14,64 @@ request(url, function (error, response, responseHtml) {
 
     //create the cheerio object
     blogobj = {},
-        //set a reference to the document that came back
-        $ = cheerio.load(responseHtml),
-        //create a reference to the meta elements
-        $title = $('head title').text(),
-        $intro = $('meta[property="og:description"]').attr('content'),
-        $author = $('meta[name="twitter:creator"]').attr('content'),
-        $author = $('footer.blog-post-meta').children().eq(0).text(),
-        $date = $('meta[property="article:published_time"]').attr('content'),
-        $link = $('meta[property="og:url"]').attr('content'),
-        $image = $('meta[property="og:image"]').attr('content'),
-        $tags = $('meta[property="article:tag"]');
+    //set a reference to the document that came back
+    $ = cheerio.load(responseHtml),
+    //create a reference to the meta elements
+    $title = $('head title').text(),
+    $intro = $('meta[property="og:description"]').attr('content'),
+    $author = $('meta[name="twitter:creator"]').attr('content'),
+    $author = $('footer.blog-post-meta').children().eq(0).text(),
+    $date = $('meta[property="article:published_time"]').attr('content'),
+    $link = $('meta[property="og:url"]').attr('content'),
+    $image = $('meta[property="og:image"]').attr('content'),
+    $tags = $('meta[property="article:tag"]');
+    console.log($tags.length);
 
     if ($title) {
-        blogobj.title = $title;
-    }
+      blogobj.title = $title;
+    } else {
+      blogobj.title = "";
+    };
 
     if ($intro) {
         blogobj.intro = $intro;
-    }
-
+      } else {
+        blogobj.intro = "";
+      };
+  
     if ($author) {
         blogobj.author = $author;
-    }
-
+      } else {
+        blogobj.author = "";
+      };
+  
     if ($date) {
         const formatter = new Intl.DateTimeFormat('en', { month: "short", day: "2-digit", year: "numeric" });
         blogobj.date = formatter.format(new Date($date));
-    }
-
+      } else {
+        blogobj.date = "";
+      };
+  
     if ($link) {
         blogobj.link = $link;
-    }
-
+      } else {
+        blogobj.link = "";
+      };
+  
     if ($image) {
         blogobj.image = $image;
-    }
-
-    if ($tags) {
-        let mySet = new Set()
+      } else {;
+        blogobj.image = "";
+      };
+  
+    if ($tags && $tags.length > 0) {
+        let mySet = new Set();
         for (var i = 0; i < $tags.length; i++) {
             mySet.add($($tags[i]).attr('content'));
         }
-        blogobj.tags = Array.from(mySet)
-    }
-    console.log(JSON.stringify(blogobj, null, '\t'));
+        blogobj.tags = Array.from(mySet);
+      } else {
+        blogobj.tags = "";
+      };
+      console.log(JSON.stringify(blogobj, null, '\t'));
 }) ;
